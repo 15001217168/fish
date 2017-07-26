e16bd8c9406c833702b91cf5110a8c1fce48ffcf
刘雪松 <liuxuesong@zccninfo.com>
2017-07-26 10:38:30 +0800

    添加command

---
+++ index.js
@@ -0,0 +1,8 @@
+/**
+ * Module dependencies.
+ */
+var root = require('./lib/domain/aggregateroot');
+var command = require('./lib/command/command');
+
+module.exports.root = root;
+module.exports.command = command;
